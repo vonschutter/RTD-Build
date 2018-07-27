0fe08551b29326ac0229678911964ac7145185df
Stephan Schutter <34268921+vonschutter@users.noreply.github.com>
2018-07-27 19:58:21 +0200

    Create rtd-oem-windows-config.cmd

---
+++ rtd-oem-windows-config.cmd
@@ -0,0 +1,25 @@
+:: --    --
+:: Windows CMD Shell Script Section
+::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Variables and Assignments
+:: Passed From CONSOLE!
+::		%0 		-> %Scriptname%
+:: Common TS
+::		%DEBUG% 	-> 1 value to turn on tracing
+::		%ECHO% 		-> On Value to turn on echo
+::		%RET% 		-> Argument Passing Value
+::
+:: 	Please list command files to be run here in the following format:
+::
+:: 	:TITLE
+:: 	Description of the pupose of called command file.
+:: 	call <path>\command.cmd or command...
+::
+::
+::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+@echo Cinfiguring windows pre 7...
