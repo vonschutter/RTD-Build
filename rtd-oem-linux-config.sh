fbae1ebf9f12fbb6a8737c080351d4c00da85703
Vonschutter <34268921+vonschutter@users.noreply.github.com>
2019-03-19 20:17:59 +0100

    Update rtd-oem-linux-config.sh

--- rtd-oem-linux-config.sh
+++ rtd-oem-linux-config.sh
@@ -336,6 +336,84 @@ options=(1 "Base RTD OEM Productivity Software" on
 				# Configure Gnome settings for current user.
 					gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 					gsettings set org.gnome.desktop.interface gtk-theme “Mytheme”
+
+					sudo systemctl enable --now tuned
+					sudo tuned-adm profile balanced
+					#Performance:
+					#sudo tuned-adm profile desktop
+					#Virtual Machine Host:
+					#sudo tuned-adm profile virtual-host
+					#Virtual Machine Guest:
+					#sudo tuned-adm profile virtual-guest
+					#Battery Saving:
+					#sudo tuned-adm profile powersave
+					# Virtual Machines
+					# sudo systemctl enable --now libvirtd
+					# Management of local/remote system(s) - available via http://localhost:9090
+					# sudo systemctl enable --now cockpit.socket
+
+					###
+					# Theming and GNOME Options
+					###
+
+					# Tilix Dark Theme
+					gsettings set com.gexperts.Tilix.Settings theme-variant 'dark'
+
+					#Gnome Shell Theming
+					gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
+					gsettings set org.gnome.desktop.interface cursor-theme 'Breeze_Snow'
+					gsettings set org.gnome.desktop.interface icon-theme 'Pop'
+					gsettings set org.gnome.shell.extensions.user-theme name 'Arc-Dark-solid'
+
+					#Set SCP as Monospace (Code) Font
+					gsettings set org.gnome.desktop.interface monospace-font-name 'Source Code Pro Semi-Bold 12'
+
+					#Set Extensions for gnome
+					gsettings set org.gnome.shell enabled-extensions "['user-theme@gnome-shell-extensions.gcampax.github.com', 'TopIcons@phocean.net', 'dash-to-dock@micxgx.gmail.com']"
+
+					#Better Font Smoothing
+					gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing 'rgba'
+
+					#Usability Improvements
+					gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'adaptive'
+					gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
+					gsettings set org.gnome.desktop.calendar show-weekdate true
+					gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+					gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+					gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
+
+					#Dash to Dock Theme
+					gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme false
+					gsettings set org.gnome.shell.extensions.dash-to-dock custom-background-color false
+					gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-customize-running-dots true
+					gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-color '#729fcf'
+					gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink true
+					gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+					gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true
+					gsettings set org.gnome.shell.extensions.dash-to-dock force-straight-corner false
+					gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-fixed true
+					gsettings set org.gnome.shell.extensions.dash-to-dock intellihide-mode 'ALL_WINDOWS'
+					gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
+					gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
+					gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items false
+					gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'FIXED'
+					gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'SEGMENTED'
+					gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.70000000000000000
+
+					#This indexer is nice, but can be detrimental for laptop users battery life
+					gsettings set org.freedesktop.Tracker.Miner.Files index-on-battery false
+					gsettings set org.freedesktop.Tracker.Miner.Files index-on-battery-first-time false
+					gsettings set org.freedesktop.Tracker.Miner.Files throttle 15
+
+					#Nautilus (File Manager) Usability
+					gsettings set org.gnome.nautilus.icon-view default-zoom-level 'standard'
+					gsettings set org.gnome.nautilus.preferences executable-text-activation 'ask'
+					gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+					gsettings set org.gnome.nautilus.list-view use-tree-view true
+
+					# Transfer settings for all users... 
+					cp -r ~/.* /etc/skel/
+
 				;;
 		esac
 	done
